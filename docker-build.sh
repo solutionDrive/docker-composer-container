d72d91e5d4c78d4fe1abae9814cdaab5dad745ab
Matthias Alt <alt@solutiondrive.de>
2019-07-02 15:15:21 +0200

    added script for building docker images

---
+++ docker-build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+docker build \
+    --build-arg PHP_VERSION=$PHP_VERSION \
+    --build-arg COMPOSER_VERSION=$COMPOSER_VERSION \
+    -t solutiondrive/docker-composer-container:php$PHP_VERSION \
+    .
+
+# Tag "latest"
+if [ "$LATEST" = "1" ]; then
+    docker tag \
+        solutiondrive/docker-composer-container:php$PHP_VERSION \
+        solutiondrive/docker-composer-container:latest
+fi
