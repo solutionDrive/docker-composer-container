a8966dc3b413a83f4921fa6314c851d2cd2d1e22
Tobias Lückel <lueckel@solutiondrive.de>
2020-01-07 17:44:51 +0100

    Adds specs for the php part

--- docker-test.sh
+++ docker-test.sh
@@ -6,6 +6,7 @@ ATTRIBUTES_TEMPLATE_FILE="tests/inspec/composer-container/attributes.yml.templat
 ATTRIBUTES_FILE="tests/inspec/composer-container/attributes.yml"
 cp ${ATTRIBUTES_TEMPLATE_FILE} ${ATTRIBUTES_FILE}
 printf '%s\n' ",s~{{ composer_version }}~${COMPOSER_VERSION}~g" w q | ed -s "${ATTRIBUTES_FILE}"
+printf '%s\n' ",s~{{ php_version }}~${PHP_VERSION}~g" w q | ed -s "${ATTRIBUTES_FILE}"
 
 DOCKER_CONTAINER_ID=`docker run -d solutiondrive/docker-composer-container:php$PHP_VERSION`
 bundle exec inspec exec tests/inspec/composer-container --attrs tests/inspec/composer-container/attributes.yml -t docker://${DOCKER_CONTAINER_ID}
